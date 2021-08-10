15815f5f0e5418ac761b80b9e4fd9503de4617bb
akrabal <akrabalgodwina@gmail.com>
2021-08-10 17:23:26 +0000

    roles

---
+++ migrations/20210810155513-create-roles.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('roles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      typeRole: {
+        type: Sequelize.STRING
+      },
+      descRoles: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('roles');
+  }
+};
